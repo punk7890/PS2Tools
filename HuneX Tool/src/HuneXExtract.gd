81b3f201e045c8980d3724490395fbea76ec94cd
punk7890 <90301698+punk7890@users.noreply.github.com>
2024-08-22 04:00:16 -0700

    Update image support for Princess Holiday

--- HuneX Tool/src/HuneXExtract.gd
+++ HuneX Tool/src/HuneXExtract.gd
@@ -16,6 +16,8 @@ var decomp_file:bool = true
 var make_tga:bool = true
 var debug_output:bool = false
 
+var offset:int #used for image output in processImg
+
 func _ready():
 	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
 	
@@ -52,10 +54,7 @@ func _on_load_exe_file_selected(path: String) -> void:
 	elf_file = path
 	
 func _on_decompress_files_pressed():
-	if decomp_file:
-		decomp_file = false
-	elif !decomp_file:
-		decomp_file = true
+	decomp_file = !decomp_file
 		
 func _on_convert_tga_pressed() -> void:
 	make_tga = !make_tga
@@ -78,6 +77,8 @@ func hunexMakeFiles() -> void:
 	var test_name:int
 	var file_extension:String
 	var elf_name:String
+	var i:int
+	var png:Image
 	
 	elf_name = elf_file.get_file()
 	loaded_array_size = bin_files.size()
@@ -231,46 +232,23 @@ func hunexMakeFiles() -> void:
 								out_file.store_buffer(mem_new_file)
 								out_file.close()
 								
+								files += 1
+								start_off += 4
+								print("0x%X " % file_off, "0x%X " % file_size, "%s" % files, "%s" % file_extension)
+								continue
+								
 							mem_file.clear()
 							
-							var image_type:int = 1
-							var has_palette:bool = true
-							var bits_per_color:int = 32
-							var bpp:int = 8
-							var width:int = mem_new_file.decode_u16(2)
-							var height:int = mem_new_file.decode_u16(4)
-							var tga_header:PackedByteArray = makeTGAHeader(has_palette, image_type, bits_per_color, bpp, width, height)
-							
-							final_image.append_array(tga_header)
-							tga_header.clear()
-							
-							var image:PackedByteArray = mem_new_file.slice(mem_new_file.size() - 0x410, - 0x10) #pallete first
-							image = unswizzle_palette(image)
-							
-							var swap:PackedByteArray
-							swap.resize(4)
-							# swap BGR to RGB and leave alpha channel
-							for k in range(0, image.size(), 4):
-								swap[0] = image.decode_u8(k)
-								swap[1] = image.decode_u8(k + 1)
-								swap[2] = image.decode_u8(k + 2)
-								image.encode_u8(k, swap[2])
-								image.encode_u8(k + 1, swap[1])
-								image.encode_u8(k + 2, swap[0])
-							swap.clear()
-							
-							final_image.append_array(image)
-							image.clear()
-							
-							image = mem_new_file.slice(0x20, mem_new_file.size() - 0x430) #image data
-							final_image.append_array(image)
-							image.clear()
-							
-							out_file = FileAccess.open(folder_path + "/%s" % files + "%s" % file_extension + ".TGA", FileAccess.WRITE)
-							out_file.store_buffer(final_image)
-							out_file.close()
-							final_image.clear()
-							mem_new_file.clear()
+							offset = 0
+							i = 0
+							while offset < mem_new_file.size():
+								if mem_new_file.decode_u16(offset) == 0:
+									break
+								png = processImg(mem_new_file)
+								png.save_png(folder_path + "/%s" % files + "%s" % file_extension + "_" + str(i) + ".png")
+								i += 1
+							i = 0
+							offset = 0
 						else:
 							out_file = FileAccess.open(folder_path + "/%s" % files + "%s" % file_extension, FileAccess.WRITE)
 							out_file.store_buffer(mem_file)
@@ -282,47 +260,19 @@ func hunexMakeFiles() -> void:
 							file_extension = ".scr"
 						elif files in range(0, 4):
 							file_extension = ".pss"
-						elif files == 4 or files in range(1147, 1189):
+						elif files == 4 or files in range(1147, 1187):
 							file_extension = ".ltex"
-							if files in range(1147, 1157) or files in range(1160, 1167): #not sure how to handle the other images
-								var image_type:int = 1
-								var has_palette:bool = true
-								var bits_per_color:int = 32
-								var bpp:int = 8
-								var width:int = mem_file.decode_u16(2)
-								var height:int = mem_file.decode_u16(4)
-								var tga_header:PackedByteArray = makeTGAHeader(has_palette, image_type, bits_per_color, bpp, width, height)
-								
-								final_image.append_array(tga_header)
-								tga_header.clear()
-								
-								var image:PackedByteArray = mem_file.slice(mem_file.size() - 0x7C0, - 0x3C0) #some odd data after the palette
-								image = unswizzle_palette(image)
-								
-								var swap:PackedByteArray
-								swap.resize(4)
-								# swap BGR to RGB and leave alpha channel
-								for k in range(0, image.size(), 4):
-									swap[0] = image.decode_u8(k)
-									swap[1] = image.decode_u8(k + 1)
-									swap[2] = image.decode_u8(k + 2)
-									image.encode_u8(k, swap[2])
-									image.encode_u8(k + 1, swap[1])
-									image.encode_u8(k + 2, swap[0])
-								swap.clear()
-								
-								final_image.append_array(image)
-								image.clear()
-								
-								image = mem_file.slice(0x20, mem_file.size() - 0x7E0) #image data
-								final_image.append_array(image)
-								image.clear()
-								#mem_file.clear()
-								
-								out_file = FileAccess.open(folder_path + "/%s" % files + "%s" % file_extension + ".TGA", FileAccess.WRITE)
-								out_file.store_buffer(final_image)
-								out_file.close()
-								final_image.clear()
+							
+							offset = 0
+							i = 0
+							while offset < mem_file.size():
+								if mem_file.decode_u16(offset) == 0:
+									break
+								png = processImg(mem_file)
+								png.save_png(folder_path + "/%s" % files + "%s" % file_extension + "_" + str(i) + ".png")
+								i += 1
+							i = 0
+							offset = 0
 						else:
 							file_extension = ".bin"
 							
@@ -396,7 +346,62 @@ func gplDataSgi(input_data: PackedByteArray) -> PackedByteArray:
 			v0 = input_data.decode_s8(a0)
 			
 	return output_data
-
+	
+func processImg(data:PackedByteArray) -> Image:
+	# Original function by Irdkwia from Python script
+	var image:PackedByteArray
+	
+	assert(data.decode_u16(offset) == 0x3254)
+	var w:int = data.decode_u16(offset + 2)
+	var h:int = data.decode_u16(offset + 4)
+	var bpp:int = data.decode_u16(offset + 6)
+	var imgdat:PackedByteArray = data.slice(offset + 0x20, offset + 0x20 + (w * h * bpp / 8))
+	imgdat = tobpp(imgdat, bpp)
+	offset += 0x20+w*h*bpp/8
+	
+	assert(data.decode_u16(offset) == 0x3254)
+	
+	var c:int = 4*data.decode_u16(offset + 2)*data.decode_u16(offset + 4)
+	var paldat:PackedByteArray = data.slice(offset + 0x20, offset + 0x20 + (w * h * bpp / 8))
+	
+	for x in range(0, len(paldat), 4):
+		paldat[x+3] = min(255, paldat[x+3]*2)
+		
+	offset += 0x20+c
+	var resdata:PackedByteArray
+	for y in range(h):
+		for x in range(w):
+			var index:int = imgdat[y * w + x] * 4
+			var end_index:int = index + 4
+			resdata.append_array(paldat.slice(index, end_index))
+	#image.append_array(imgdat)
+	#image.append_array(paldat)
+	#image.append_array(resdata)
+	var png:Image = Image.create_from_data(w, h, false, 5, resdata)
+	#png.save_png("C:/Users/punk_/Desktop/test/1159.png")
+	return png
+	
+func tobpp(data:PackedByteArray, bpp:int) -> PackedByteArray:
+	# Original function by Irdkwia from Python script
+	
+	var out:PackedByteArray
+	var p:int
+	
+	if bpp not in [1, 2, 4, 8]:
+		push_error("Unsupported BPP %s " % bpp)
+		
+	var m:int = (1<<bpp)-1
+	for b in data:
+		for x in range(8/bpp):
+			if bpp==8:
+				var swizzle:int = b&m
+				p = (swizzle&0xE7)|((swizzle&0x10)>>1)|((swizzle&0x8)<<1)
+			else:
+				p = b&m
+			out.append(p)
+			b>>=bpp
+			
+	return out
 func makeTGAHeader(has_palette:bool, image_type:int, bits_per_color:int, bpp:int, width:int, height:int) -> PackedByteArray:
 	var header:PackedByteArray
 	var num_palette_entries:int = 1
